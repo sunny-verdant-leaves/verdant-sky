4d63845ac3dc64571cc04cedb0c4fecd6a3e5069
sunny-verdant-leaves <243575605+sunny-verdant-leaves@users.noreply.github.com>
2026-09-17 20:39:25 +0800

    rename pack

---
+++ common/src/main/java/com/github/verdant_leaves/verdant_sky/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.github.verdant_leaves.verdant_sky;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "verdant_sky";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ forge/src/main/java/com/github/verdant_leaves/verdant_sky/forge/ExampleModForge.java
@@ -0,0 +1,18 @@
+package com.github.verdant_leaves.verdant_sky.forge;
+
+import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import com.github.verdant_leaves.verdant_sky.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModForge {
+    public ExampleModForge() {
+        // Submit our event bus to let Architectury API register our content on the right time.
+        EventBuses.registerModEventBus(ExampleMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
