67277fcfc17426e0fac7ad48580e056e7aec74cc
sunny-verdant-leaves <243575605+sunny-verdant-leaves@users.noreply.github.com>
2026-09-17 20:41:58 +0800

    clear

--- common/src/main/java/verdant_leaves/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package verdant_leaves;
-
-public final class ExampleMod {
-    public static final String MOD_ID = "verdant_sky";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
