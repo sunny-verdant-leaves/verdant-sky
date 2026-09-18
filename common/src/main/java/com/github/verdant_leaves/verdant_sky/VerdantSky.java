01d9b9b9ca2ef24f72a2f1103c9d456162f5bbe2
碧叶晴天 <Biye235@163.com>
2026-09-18 06:40:59 +0000

    Revert "VerdantPoolBlockEntity"
    This reverts commit 268af63c76ce06348d60c11445b13ff9f8f16a78.

--- common/src/main/java/com/github/verdant_leaves/verdant_sky/VerdantSky.java
+++ common/src/main/java/com/github/verdant_leaves/verdant_sky/VerdantSky.java
@@ -1,13 +1,11 @@
 package com.github.verdant_leaves.verdant_sky;
 
 import com.github.verdant_leaves.verdant_sky.registry.ModBlocks;
-import com.github.verdant_leaves.verdant_sky.registry.ModBlockEntities;
 
 public final class VerdantSky {
     public static final String MOD_ID = "verdant_sky";
 
     public static void init() {
         ModBlocks.register();
-        ModBlockEntities.register();
     }
 }
--- common/src/main/java/com/github/verdant_leaves/verdant_sky/block/VerdantPoolBlock.java
+++ common/src/main/java/com/github/verdant_leaves/verdant_sky/block/VerdantPoolBlock.java
@@ -1,35 +1,10 @@
 package com.github.verdant_leaves.verdant_sky.block;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityTicker;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
-
 import vazkii.botania.common.block.mana.ManaPoolBlock;
-
-import com.github.verdant_leaves.verdant_sky.block.entity.VerdantPoolBlockEntity;
-import com.github.verdant_leaves.verdant_sky.registry.ModBlockEntities;
 
 public class VerdantPoolBlock extends ManaPoolBlock {
     public VerdantPoolBlock(BlockBehaviour.Properties props) {
         super(Variant.DILUTED, props);
-    }
-
-    @Override
-    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        return new VerdantPoolBlockEntity(pos, state);
-    }
-
-    @Nullable
-    @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
-        return createTickerHelper(type, ModBlockEntities.VERDANT_POOL.get(),
-            level.isClientSide
-                ? VerdantPoolBlockEntity::clientTick
-                : VerdantPoolBlockEntity::serverTick);
     }
 }
